24b05721ed74af0eec8e864b533ab07da42c67ac
Mark Ruddy <1markruddy@gmail.com>
2022-11-15 08:15:53 +0000

    remove target directory from rust backend

--- backend/target/debug/deps/backend-88762b767feda09f.d
+++
@@ -1,5 +0,0 @@
-/home/mark/dev/neurorecovery/backend/target/debug/deps/backend-88762b767feda09f.rmeta: src/main.rs
-
-/home/mark/dev/neurorecovery/backend/target/debug/deps/backend-88762b767feda09f.d: src/main.rs
-
-src/main.rs:
--- backend/target/debug/deps/backend-b972b614d57f1644.d
+++
@@ -1,5 +0,0 @@
-/home/mark/dev/neurorecovery/backend/target/debug/deps/backend-b972b614d57f1644.rmeta: src/main.rs
-
-/home/mark/dev/neurorecovery/backend/target/debug/deps/backend-b972b614d57f1644.d: src/main.rs
-
-src/main.rs:
