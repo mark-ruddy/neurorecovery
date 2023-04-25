fbdee145e5b57f233aef959973d1044ba8191701
Mark Ruddy <1markruddy@gmail.com>
2023-04-25 15:10:01 +0100

    update rdp python server

--- rdp_matlab_local_server/launch_rdp_matlab.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def launch_matlab_remmina(rdp_conn):
-    subprocess.call(f"remmina -c '{rdp_conn}'", shell=True)
-
-def launch_matlab_rdp(url):
-    # TODO: need option to find if OS is windows and not use remmina
-    launch_matlab_remmina(url)
--- rdp_matlab_local_server/server.py
+++ rdp_matlab_local_server/server.py
@@ -1,16 +1,19 @@
 import os
+import subprocess
+import platform
 from flask import Flask, jsonify
 from os.path import join, dirname
 from dotenv import load_dotenv
-from launch_rdp_matlab import *
 
 app = Flask(__name__)
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 REMMINA_PASS = os.environ.get("REMMINA_PASS")
+RDP_PASS = os.environ.get("RDP_PASS")
 
-rdp_conn = f"rdp://Administrator:{REMMINA_PASS}@ec2-35-179-74-36.eu-west-2.compute.amazonaws.com"
+windows_rdp_cmd = f"mstsc /v:ec2-35-179-74-36.eu-west-2.compute.amazonaws.com"
+linux_rdp_cmd = f"remmina -c rdp://Administrator:{REMMINA_PASS}@ec2-35-179-74-36.eu-west-2.compute.amazonaws.com"
 
 @app.route('/')
 def index():
@@ -18,13 +21,21 @@ def index():
 
 @app.route('/launch_matlab_rdp')
 def launch_rdp_matlab_api():
-    launch_matlab_rdp(rdp_conn)
+    if platform.system() == "windows":
+        subprocess.call(f'cmdkey /generic:"ec2-35-179-74-36.eu-west-2.compute.amazonaws.com" /user:"Administrator" /pass:"{RDP_PASS}"', shell=True);
+        launch_matlab_rdp(windows_rdp_cmd)
+    else:
+        launch_matlab_rdp(linux_rdp_cmd)
 
     response = jsonify("Matlab RDP should be launched")
     response.headers.add("Access-Control-Allow-Origin", "*")
     response.headers.add("Access-Control-Request-Headers", "X-Requested-With, accept, content-type")
     response.headers.add("Access-Control-Allow-Methods", "GET")
     return response
+
+def launch_matlab_rdp(cmd):
+    # TODO: need option to find if OS is windows and not use remmina
+    subprocess.call(cmd, shell=True)
 
 # NOTE: For the eduroam connection, instead of a DNS name it'll be an IP addr like below, and matlab username, will need to encrypt the password too and add it to the .env file:
 # rdp_conn = f"rdp://matlab:{REMMINA_PASS}@1.1.1.1"
